10b5402bd67415cbdbcc59c336710287f00b2fe2
pavlogorbunov <pavlogorbunov@yandex.ru>
2023-09-10 21:37:27 +0300

    ConeApproximationFrontend 1.1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { VertexNormalsHelper } from 'three/addons/helpers/VertexNormalsHelper.js';
 import './style.css'
 
 const inputs = document.querySelectorAll('.menu__input')
@@ -16,17 +17,24 @@ const coneMaterial = new THREE.MeshStandardMaterial({
     color: 0x303030,
 })
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 2)
-directionalLight.position.set(10, -20, 5)
+const directionalLight = new THREE.DirectionalLight(0xffffff, 5)
+directionalLight.position.set(10, -10, 10)
 scene.add(directionalLight)
+// const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5)
+// scene.add(directionalLightHelper)
 
 const light = new THREE.AmbientLight(0xffffff, 5)
 scene.add(light)
 
 const controls = new OrbitControls(camera, renderer.domElement)
-camera.position.set(0, 10, -3)
+camera.position.set(0, -3, 7)
 camera.lookAt(0, 0, 0)
 controls.update()
+
+// const axesHelper = new THREE.AxesHelper(5)
+// scene.add(axesHelper)
+
+let normalsHelper
 
 let t1 = Date.now()
 
@@ -38,13 +46,14 @@ function animate() {
     requestAnimationFrame(animate)
     coneGeometry.rotateZ(0.0001 * dt)
     controls.update()
+    if (normalsHelper) normalsHelper.update()
     renderer.render(scene, camera)
 }
 
-function getConeDataFromBackend() {
+async function getConeData(parameters) {
     fetch('http://localhost:3000', {
         method: 'POST',
-        body: JSON.stringify({ R: 1, N: 10, H: 5 }),
+        body: JSON.stringify(parameters),
         mode: 'cors',
         headers: {
             "Content-Type": "application/json",
@@ -54,10 +63,6 @@ function getConeDataFromBackend() {
         .then(data => {
             coneGeometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(data.pointsArray), 3))
             coneGeometry.setAttribute('normal', new THREE.BufferAttribute(new Float32Array(data.normalsArray), 3))
-
-            const cone = new THREE.Mesh(coneGeometry, coneMaterial)
-            cone.position.z = -1
-            scene.add(cone)
         })
         .catch(res => {
             alert('Fetch error')
@@ -90,19 +95,7 @@ function onInputChange(evt) {
             }
         })
         if (formIsValid) {
-            fetch('http://localhost:3000', {
-                method: 'POST',
-                body: JSON.stringify(reqBody),
-                mode: 'cors',
-                headers: {
-                    "Content-Type": "application/json",
-                }
-            })
-                .then(res => res.json())
-                .then(data => {
-                    coneGeometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(data.pointsArray), 3))
-                    coneGeometry.setAttribute('normal', new THREE.BufferAttribute(new Float32Array(data.normalsArray), 3))
-                })
+            getConeData(reqBody)
         }
     } else {
         evt.target.classList.add('menu__input_incorrect')
@@ -115,8 +108,14 @@ inputs.forEach(el => {
 
 window.addEventListener('resize', onResize)
 
-// setTimeout(getConeDataFromBackend, 5000)
+// setTimeout(getConeData, 5000)
 
-getConeDataFromBackend()
+getConeData({ R: 1, H: 5, N: 10, S: false }).then(() => {
+    const cone = new THREE.Mesh(coneGeometry, coneMaterial)
+    // normalsHelper = new VertexNormalsHelper(cone, .3, 0x00ff00)
+    cone.position.z = -1
+    scene.add(cone)
+    // scene.add(normalsHelper)
+})
 
 animate()
